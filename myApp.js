1eb212796d2a29a73bcea59cac7a4adbd83144b9
FavourtheDev <favourthedeveloper@gmail.com>
2023-05-03 02:29:13 +0100

    Adding Project Files

---
+++ myApp.js
@@ -0,0 +1,39 @@
+const inputText = document.getElementById("input");
+const list = document.getElementById("list");
+
+function addList() {
+        var http = new XMLHttpRequest();
+        http.onreadystatechange = function() {
+            if(this.readyState === 4) {
+                if(this.status === 200) {
+                    var data = JSON.parse(this.responseText);
+                    for(var i = 0; i < data.length; i++) {
+                         let todo = data[i].title;
+                        let newList = document.createElement("li");
+                        newList.innerText = todo;
+                        list.appendChild(newList);
+
+                         newList.addEventListener('click', function() {
+        newList.style.textDecoration = "line-through";
+        newList.style.color = "red";
+        
+    })
+    
+    newList.addEventListener('dblclick', function() {
+        list.removeChild(newList);
+    })
+                        
+                    }
+                }
+            }
+        }
+        http.open('GET', 'https://jsonplaceholder.typicode.com/todos', true);
+        http.send();
+   
+
+
+}
+     
+
+
+
